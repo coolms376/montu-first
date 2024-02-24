9875edb7fe134b76fa223e94d64fce215ca746a4
Montu <you@example.com>
2024-02-25 01:50:34 +0530

    cors added

--- src/main/java/com/school/mailservice/ApplicationConfiguration.java
+++ src/main/java/com/school/mailservice/ApplicationConfiguration.java
@@ -66,7 +66,7 @@ public class ApplicationConfiguration implements WebMvcConfigurer {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/greeting-javaconfig").allowedOrigins("*");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
